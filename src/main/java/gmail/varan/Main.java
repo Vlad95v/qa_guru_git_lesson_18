37fe7abdc8641741cf1d0bf38ad342d208a4a4c9
varan <bjiad22@gmail.com>
2026-07-25 01:01:28 +0300

    add slow

--- src/main/java/gmail/varan/Main.java
+++ src/main/java/gmail/varan/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-
         System.out.printf("Hello slow work !");
+        System.out.printf("Hello slow work !!");
 
 
         for (int i = 1; i <= 5; i++) {
